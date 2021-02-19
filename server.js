84ce95dcf19649d46bc4c4475b8f80795fcc0f0e
khoaHyh <khoahuynh.toronto@gmail.com>
2021-02-19 17:46:55 -0500

    Feat: Add post endpoint
    Add dns lookup to verify a submitted url. Handle POST requests with
    bodyparser.

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+
+// Implement a Root-Level Request Logger Middleware
+app.use((req, res, next) => {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
+
+// Use body-parser to Parse POST Requests
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -10,15 +25,19 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => {
+  	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl/new', (req, res) => {
+	let original = req.body.url;
+	dns.lookup(original, (err, add, fam) => {
+		if (err) return console.error(`dns lookup error: ${err}`);
+		console.log(`add: ${add}, fam: ${fam}`);
+	});
+  	res.status(200).json({ original_url: original, short_url: req.body.url  });
 });
 
-app.listen(port, function() {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
